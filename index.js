74e88d48d714105878b3f701a07a97d10be3da76
ajhar10 <ajharrimon90@gmail.com>
2022-05-07 04:12:52 +0600

    one product load

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 const express = require('express');
-// require('dotenv').config();
-// const cors = require('cors');
-// const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 //Middle ware
-// app.use(cors())
-// app.use(express.json());
+app.use(cors())
+app.use(express.json());
 
 //From Mongodb Connect
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://ajhar41:hHNLa4RoV7D5rBge@cluster0.vm5vj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        const serviceCollection = client.db('tirezCycle').collection('products');
+        const productCollection = client.db('tirezCycle').collection('products');
         //find or get data
         app.get('/products', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = productCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
 
-        //find one service 
-        // app.get('/products/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const service = await serviceCollection.findOne(query);
-        //     res.send(service);
-        // })
+        //find one product 
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
 
 
     } finally {
