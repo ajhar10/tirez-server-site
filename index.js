4389b324b80c473028ea139a7b05377c71e33f1f
ajhar10 <ajharrimon90@gmail.com>
2022-05-07 02:37:53 +0600

    data retrive

--- index.js
+++ index.js
@@ -8,6 +8,41 @@ const port = process.env.PORT || 5000;
 // app.use(cors())
 // app.use(express.json());
 
+//From Mongodb Connect
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ajhar41:hHNLa4RoV7D5rBge@cluster0.vm5vj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('tirezCycle').collection('products');
+        //find or get data
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        //find one service 
+        // app.get('/products/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // })
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send("Server is running")
@@ -16,3 +51,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Tirez server is running in ", port);
 })
+
+//hHNLa4RoV7D5rBge
+//ajhar41
